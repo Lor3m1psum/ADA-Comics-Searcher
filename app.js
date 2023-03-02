2514ef3358dcb1278f31cd27eda8f6b19158b6de
Lor3m1psum <lorena.rosales.pajaro87@gmail.com>
2023-03-01 21:35:20 -0500

    last details init page

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-let spanTexts = document.getElementsByTagName("span");
+const spanTexts = document.getElementsByTagName("span");
+const btnHidden = document.getElementsByClassName("button-init");
+
 window.onload = function () {
   for (anSpan of spanTexts) {
     anSpan.classList.add("active");
